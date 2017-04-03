30175c40b9d9f112809d622d9f70a92c5baedad3
Oriol Vall <oriol.vall.hdez@gmail.com>
2017-04-04 00:07:15 +0200

    Fixed Heroku

--- music/settings.py
+++ music/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,9 +83,6 @@ DATABASES = {
 }
 
 
-DATABASES['default'] = dj_database_url.config()
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -123,9 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = ['*']
+
+DEBUG = False
 
 try:
     from .local_settings import *
